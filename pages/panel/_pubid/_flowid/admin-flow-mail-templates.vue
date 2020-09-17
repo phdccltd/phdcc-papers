0f9e167c50a94f3a237fe04b3d51fa8cdad329ee
chriscant <chris@phdcc.com>
2020-09-17 15:50:31 +0100

    Tweak template substitutions

--- pages/panel/_pubid/_flowid/admin-flow-mail-templates.vue
+++ pages/panel/_pubid/_flowid/admin-flow-mail-templates.vue
@@ -163,7 +163,7 @@
       substitutions() {
         let substitutions = '{{submit.id}}\r{{submit.name}}\r{{user.username}}\r{{user.id}}\r{{now}}\r\r'
         substitutions += '{{author.username}}\r{{author.id}}\r\r'
-        substitutions += '{{grading.score}}\r{{grading.comments}}\r{{grading.canreview}}\r\r'
+        substitutions += '{{grading.score}}\r{{grading.comment}}\r{{grading.canreview}}\r\r'
         substitutions += '{{entry.id}}\r'
         const flow = this.flow
         for (const stage of flow.stages) {
