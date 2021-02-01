d27b594bda15616c318a551f8201dbb4785f96c3
chriscant <chris@phdcc.com>
2021-02-01 12:41:17 +0000

    Bump ver. Tweak for sonarcloud

--- layouts/default.vue
+++ layouts/default.vue
@@ -34,9 +34,9 @@
       <a href="https://www.phdcc.com/papers/">Papers {{ version }}/{{apiversion}}</a>,
       <span style="color:lightgrey">
         built: {{BUILD_DATE}}.
-        Copyright &copy; 2020 PHD Computer Consultants Ltd.
+        Copyright &copy; 2021 PHD Computer Consultants Ltd.
       </span><br />
-      <a href="https://www.phdcc.com/feedback.html" target="_blank">Get in touch if you want to try out Papers for your conference or journal.</a>
+      <a href="https://www.phdcc.com/feedback.html" target="_blank" rel="noopener">Get in touch if you want to try out Papers for your conference or journal.</a>
     </div>
   </div>
 </template>
