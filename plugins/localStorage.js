04c7390152900ca6a886257326f11cb23e962327
Chris Cant <chris@phdcc.com>
2022-07-27 17:02:05 +0100

    Update packages on node 14

--- plugins/localStorage.js
+++ plugins/localStorage.js
@@ -1,27 +1,11 @@
 // Runs in client
 // https://github.com/robinvdvleuten/vuex-persistedstate
 
-import createPersistedState from 'vuex-persistedstate'
-
-const STORAGE = window.localStorage
-const STORAGE_KEY = 'papers'
+import VuexPersistence from 'vuex-persist'
 
 export default ({ store }) => {
-  console.log('==================== localStorage')
-  // Before leaving the page, make sure we save the state
-  window.addEventListener('beforeunload', () => {
-    // console.log('Save state before unload - NO ACTION FOR NOW')
-    // if (settingState) {
-    //  trySaving(STORAGE, settingState)
-    // }
-  })
-
-  createPersistedState({
-    key: STORAGE_KEY,
-    storage: STORAGE
-    /* paths: [
-      'auth.user',
-      'helpboxes.visible',
-    ] */
-  })(store)
+  new VuexPersistence({
+    key: 'papers',
+    storage: window.localStorage
+  }).plugin(store);
 }
