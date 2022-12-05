006f695589e8e17027048e56646478b3d6182b07
Chris Cant <chris@phdcc.com>
2022-12-05 17:45:08 +0000

    Start add stage

---
+++ pages/panel/[pubid]/[flowid]/[submitid]/add/[stageid]/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <!-- ADD ENTRY TO SUBMIT -->
+    <EntryForm formtype="addstage" />
+  </div>
+</template>
+
+<script lang="ts">
+import EntryForm from '~/components/EntryForm.vue'
+
+export default {
+  middleware: 'authuser',
+  components: { EntryForm },
+  data() {
+    return {
+    }
+  },
+}
+</script>
