6d1ddfaf179828ef989fbee57d9eb4325bfe5995
Chris Cant <chris@phdcc.com>
2022-12-13 13:46:30 +0000

    Fix up auth recaptcha and show errors

--- composables/useVueRecaptcha.js
+++ composables/useVueRecaptcha.js
@@ -6,7 +6,6 @@ export const useVueRecaptcha = async () => {
   const { vueApp } = useNuxtApp();
   const runtimeConfig = useRuntimeConfig()
   const siteKey = runtimeConfig.public.RECAPTCHA_SITE_KEY;
-  //console.log('useVueRecaptcha.js',siteKey)
 
   vueApp.use(VueReCaptcha, {
     siteKey: siteKey,
