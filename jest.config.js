4bc7326340c35078619f3817609993c759377149
Chris Cant <chris@phdcc.com>
2022-12-21 16:54:36 +0000

    Upg packages. More test testing

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,15 @@
-module.exports = {
-  preset: '@nuxt/test-utils',
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-    '^~/(.*)$': '<rootDir>/$1',
-    '^vue$': 'vue/dist/vue.common.js',
-  },
-  moduleFileExtensions: ['ts', 'js', 'vue', 'json'],
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-    '^.+\\.js$': 'babel-jest',
-    //'.*\\.(vue)$': 'vue-jest',
-  },
-  collectCoverage: true,
+const { defaults } = require('jest-config');
+console.log("jest.config.js")
+/** @type {import('jest').Config} */
+const config = {
+  //preset: '@nuxt/test-utils',
+  globalSetup: './jest.once.js',
+  coverageDirectory: 'coverage',
   collectCoverageFrom: [
     '<rootDir>/components/**/*.vue',
     '<rootDir>/pages/**/*.vue'
   ],
-}
+};
+
+module.exports = config;
+
